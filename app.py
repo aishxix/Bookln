29fde21c5a67065bacdd4f481ea7ede4388e3b1b
aishxix <aishmuhammad388@gmail.com>
2025-01-14 08:24:25 +0500

    addedD&T:aish

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session
-
+from datetime import datetime
+import urllib.parse
 app = Flask(__name__)
 app.secret_key = 'aish'
 
@@ -66,17 +67,27 @@ def contact():
 @app.route('/book', methods=['GET'])
 def book_now():
     if 'username' in session:
-
+        date = request.args.get('date')
         court_name = request.args.get('court')
         time_slot = request.args.get('time')
         phone_number = request.args.get('number')
+        
+        try:
+            selected_date = datetime.strptime(date, '%Y-%m-%d')
+            if selected_date < datetime.now():
+                return 'Error: You cannot select a past date'
+        except ValueError:
+            return 'Error: Invalid date format.'
+        
 
-        if not court_name or not time_slot or not phone_number:
+        if not court_name or not time_slot or not phone_number or not date:
             return "Missing required parameters.", 400
 
-        message = f"Hello! I'm {usr_data['firstname']} {usr_data['lastname']} I would like to book the {court_name} for the {time_slot} slot. Can you confirm the availability and price?"
+        message = f"Hello! I'm {usr_data['firstname']} {usr_data['lastname']} I would like to book the {court_name} for {date} & Time Slot {time_slot} slot. Can you confirm the availability and price?"
 
-        whatsapp_url = f"https://api.whatsapp.com/send?phone={phone_number}&text={message}"
+        encoded_message = urllib.parse.quote(message)
+        
+        whatsapp_url = f"https://api.whatsapp.com/send?phone={phone_number}&text={encoded_message}"
 
         return redirect(whatsapp_url)
     else:
